a9cf4afa062a7209b113f7174ce30351ae1e0a40
Nikhil Singh <nikhils@sahaj.ai>
2025-09-10 15:47:20 +0530

    Fix: markdown | Change which key to helix style | add noice

--- lua/plugins/utils/init.lua
+++ lua/plugins/utils/init.lua
@@ -1,5 +1,7 @@
 return {
   { import = "plugins.utils.aerial" },
   { import = "plugins.utils.lazygit" },
-  { import = "plugins.utils.yanky" }
+  { import = "plugins.utils.noice" },
+  { import = "plugins.utils.yanky" },
+  { import = "plugins.utils.which-key"},
 }
